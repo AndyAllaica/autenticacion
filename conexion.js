14560c5fd01de4be438ac00b8454f8697c36e228
andrecito0207 <oviedojonathan2001@gmail.com>
2023-11-16 19:01:12 -0500

    first commit

---
+++ conexion.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+const conexion = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Jonandres07',
+    database:'prueba'
+})
+
+conexion.connect((err)=>{
+    if(err) throw err
+    console.log('Conexion establecida')
+})
+
+conexion.query('SELECT * FROM tabla WHERE idTabla=1 AND texto=\'Hola\'', (err, rows)=>{
+    if(err) throw err
+    console.log('Los datos solicitados son:')
+    console.log(rows)
+})
+
+conexion.end();
