939f08d5580e1405ef1ea4d3a7b1890711f79a04
andrecito2001 <[andrecito2001]>
2023-11-16 21:48:24 -0500

    comabisssss

--- conexion.js
+++ conexion.js
@@ -29,5 +29,29 @@ function conectarMySQL() {
 }
 
 function registro(){
-    
+    var usuario = document.getElementById("email").value
+    var contra = document.getElementById("password").value
+    const mysql = require('mysql')
+    const conexion = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Musica1234',
+        database: 'ejemplo'
+    })
+
+    conexion.connect((err) => {
+        if (err) throw err
+        console.log('Conexion establecida')
+    })
+
+    conexion.query("INSERT INTO producto ='" + usuario + "' AND contra='" + contra + "'", (err, rows) => {
+        if (err) {
+            console.error('Error en la autenticación:', err);
+            alert('El producto no se pudo añadir.');
+        } else {
+            alert('Producto añadido.');
+        }
+
+        conexion.end();
+    })
 }
