d9dddc7fe5c7f44dc5646f53dd0317a7ce8b081f
AndyAllaica <andyallaica@gmail.com>
2023-11-16 22:01:53 -0500

    CAMBIOS

--- configuraciones.js
+++ configuraciones.js
@@ -1,8 +1,26 @@
 function mostrarContrasena(){
+      var tipo = document.getElementById("password");
+      if(tipo.type == "password"){
+          tipo.type = "text";
+      }else{
+          tipo.type = "password";
+      }
+  }
+
+/*
+const pass = document.getElementById("password"),
+    icon = document.querySelector(".bx");
+
+icon.addEventListener("click",e=>{
     var tipo = document.getElementById("password");
     if(tipo.type == "password"){
         tipo.type = "text";
+        icon.classList.remove('bx-show-alt')
+        icon.classList.add('bx-hide')
     }else{
         tipo.type = "password";
+        icon.classList.add('bx-show-alt')
+        icon.classList.remove('bx-hide')
     }
-}
+})
+*/
